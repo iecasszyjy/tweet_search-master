49b25b8ad03943009ffdc2d0876f4c867b9e5fb0
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2018-08-13 14:37:45 +0800

    Create Config_defect.py

---
+++ spider/Config_defect.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# got文件、MongoDB数据库和Redis数据库配置文件
+import os
+import sys
+
+import pymongo
+import redis
+
+def get_noau_config():
+    # got文件
+    if sys.version_info[0] < 3:
+        import got
+    else:
+        import got3 as got
+
+    # MongoDB数据库
+    client = pymongo.MongoClient(os.environ['MONGOHOST'], 27017)
+    db = client.nk_defect
+
+    # Redis数据库
+    r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0)
+
+    return got, db, r
