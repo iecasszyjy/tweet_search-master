5272d264053ae1fd2f42dcddd79536799cb68c44
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2018-10-23 16:27:20 +0800

    Update Config_open.py

--- spider/Config_open.py
+++ spider/Config_open.py
@@ -14,9 +14,10 @@ def get_noau_config():
         import got3 as got
 
     # MongoDB数据库
-    client = pymongo.MongoClient(os.environ['MONGOHOST'], 27017)
+    client = pymongo.MongoClient('54.161.160.206:27017')
+    db_auth = client.admin
+    db_auth.authenticate(name='iecas', password='szyjy')
     db = client.open
-    db.authenticate(name='iecas', password='szyjy')
 
     # Redis数据库
     r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0)
