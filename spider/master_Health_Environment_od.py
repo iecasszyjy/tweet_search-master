104bff007d1aefcbd4c5e488d8a9063715f3960c
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2018-10-30 08:16:32 +0800

    Add files via upload

---
+++ spider/master_Health_Environment_od.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*
+# 获得爬取“宗教”推文所需信息
+import json
+from Config_Health_Environment_od import get_noau_config
+from datetime import datetime, timedelta
+
+_, db, r = get_noau_config()  # 数据库配置
+
+
+def get_date(date):
+    # 获取日期集合
+    dates = date.split('\n')
+    date_list = []
+    for i in dates:
+        if not len(i.strip()) == 0:
+            date_list.append(i.strip())
+    return list(set(date_list))
+
+
+def get_location(location):
+    # 获取地点集合
+    locations = location.split('\n')
+    location_list = []
+    for i in locations:
+        if not len(i.strip()) == 0:
+            location_list.append(i.strip())
+    return list(set(location_list))
+
+
+def get_triggers(trigger):
+    # 获取事件触发词集合
+    triggers = trigger.split('\n')
+    trigger_list = []
+    for i in triggers:
+        if not len(i.strip()) == 0:
+            trigger_list.append(i.strip())
+    return list(set(trigger_list))
+
+
+def get_query_str(event):
+    # 获取Twitter查询信息
+    # loc = get_location(event['event']['loc'])
+    trigger = get_triggers(event['event']['trigger'])
+    date = event['event']['date']
+    date = date.strip()
+    temp = datetime.strptime(date, "%Y-%m-%d")
+    date_since = (temp - timedelta(days=7)).strftime('%Y-%m-%d')
+    date_until = (temp + timedelta(days=7)).strftime('%Y-%m-%d')
+    # 注意查询格式必须形如(xxx OR xxx) (xxx OR xxx) since:xxxx-xx-xx until:xxxx-xx-xx   # 暂时不加地点
+    # return '(' + ' OR '.join(loc) + ')' + ' ' + '(' + ' OR '.join(trigger) + ')' + '(' + ' OR '.join(religion) + ')'\
+    #        ' ' + 'since:' + date_since + ' ' + 'until:' + date_until
+    return '(' + ' OR '.join(trigger) + ')' + ' ' + 'since:' + date_since + ' ' + 'until:' + date_until
+
+
+def get_task():
+    events = db.event_list.find({})
+    for event in events:
+        q = get_query_str(event)
+        message = {'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 10000, 'event_id': event['_id']}
+        print(message)
+        # 把获取推文所需信息放入Redis数据库
+        r.rpush('Health_Environment_od', json.dumps(message))
+    print('master_Health_Environment_od done!')
+
+
+if __name__ == '__main__':
+    get_task()
+
+
