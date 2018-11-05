ab6ec1d98b646950decbb08b3a93638728fa1678
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2018-11-05 09:12:27 +0800

    Add files via upload

---
+++ spider/Config_Disasters_Accidents_od.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+# got文件、MongoDB数据库和Redis数据库配置文件
+# 开放域Disasters_Accidents事件的配置信息
+
+import os
+import sys
+
+import pymongo
+import redis
+
+
+def get_noau_config():
+    # got文件
+    if sys.version_info[0] < 3:
+        import got
+    else:
+        import got3 as got
+
+    # MongoDB数据库
+    client = pymongo.MongoClient(os.environ['MONGOHOST'], 27017)
+    # client = pymongo.MongoClient('54.161.160.206', 27017)
+    db_auth = client.admin
+    db_auth.authenticate(name='iecas', password='szyjy')
+    db = client.Disasters_Accidents_od  # Disasters_Accidents_od数据库
+
+    # Redis数据库
+    r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0)
+    # r = redis.StrictRedis('54.161.160.206', port=6379, db=0)
+
+    return got, db, r
