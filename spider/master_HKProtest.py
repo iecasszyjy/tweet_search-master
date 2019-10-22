01513079df217c74a4eadc4aaac5ea34e4039ad9
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2019-10-22 15:38:38 +0800

    Update master_HKProtest.py

--- spider/master_HKProtest.py
+++ spider/master_HKProtest.py
@@ -7,32 +7,28 @@ from Config_2019 import get_noau_config
 _, db = get_noau_config()  # 数据库配置
 
 def get_task():
-    conditions = list(db.conditions.find())
+    event_list = list(db.event_list.find())
     stime, etime, locations, triggers = '','','',''
-    print('condition num:' + str(len(conditions)))
-    for condition in conditions:
-        print(condition)
-        print(type(condition))
-        if 'stime' in condition.keys():
-            stime = condition['stime']
-        elif 'etime' in condition.keys():
-            etime = condition['etime']
-        elif 'locations' in condition.keys():
-            locations = condition['locations']
-        elif 'triggers' in condition.keys():
-            triggers = condition['triggers']
-    q = '(' + locations + ')' + ' ' + '(' + triggers + ')' + ' ' + 'since:' + stime + ' ' + 'until:' + etime
-    actionId = hash(stime + etime + locations + triggers)
-    message = {'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 50000, 'action_id': actionId}
-    print('推文查询语句组织完成')
-    print(message)
+    print('event_list num:' + str(len(event_list)))
+    for event in event_list:
+        print(event)
+        print(type(event))
+        stime = event['event']['stime']
+        etime = event['event']['etime']
+        locations = event['event']['locations']
+        triggers = event['event']['triggers']
+        q = '(' + locations + ')' + ' ' + '(' + triggers + ')' + ' ' + 'since:' + stime + ' ' + 'until:' + etime
+        actionId = event['id']
+        message = {'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 50000, 'action_id': actionId}
+        print('推文查询语句组织完成')
+        print(message)
     # # 把获取推文所需信息放入Redis数据库
     # r.rpush('2019HongKong_protest', json.dumps(message))
     # 把获取推文所需的特定格式信息存入mongo
-    searchInfo = db.searchInfo
-    if searchInfo.find_one({'id': actionId}) is None:
-        searchInfo.insert_one({'id': actionId, 'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 50000})
-        print('推文查询语句存储到mongo/2019HongKong_protest/searchInfo完成')
+        searchInfo = db.searchInfo
+        if searchInfo.find_one({'id': actionId}) is None:
+            searchInfo.insert_one({'id': actionId, 'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 50000})
+            print('推文查询语句存储到mongo/2019HongKong_protest/searchInfo完成')
 
 if __name__ == '__main__':
     get_task()
