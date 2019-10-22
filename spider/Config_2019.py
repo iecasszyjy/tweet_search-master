16ece17cb197e3407c5d4aa2b8d6dd8e3ca4e1c0
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2019-10-22 16:48:45 +0800

    Update Config_2019.py

--- spider/Config_2019.py
+++ spider/Config_2019.py
@@ -14,7 +14,8 @@ def get_noau_config():
 
     # MongoDB数据库
     # client = pymongo.MongoClient(os.environ['MONGOHOST'], 27017, connect=False)
-    client = pymongo.MongoClient('mongodb://3.220.111.222:27017/')
+    # client = pymongo.MongoClient('mongodb://3.220.111.222:27017/')
+    client=pymongo.MongoClient('3.220.111.222:27017', 27017, connect=False)
     client.admin.authenticate("aircas", "aircas@2018", mechanism='SCRAM-SHA-1')
     # db = client.natural_disaster
     db = client['2019HongKong_protest']
