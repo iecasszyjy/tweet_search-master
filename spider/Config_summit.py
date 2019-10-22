93b036f2a75e662438ed02ce5828e8f6c1ee34fb
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2019-10-22 14:33:41 +0800

    Update Config_summit.py

--- spider/Config_summit.py
+++ spider/Config_summit.py
@@ -14,11 +14,15 @@ def get_noau_config():
         import got3 as got
 
     # MongoDB数据库
-    client = pymongo.MongoClient(os.environ['MONGOHOST'], 27017)
-    db = client.summit
-    db.authenticate(name='iecas', password='szyjy')
+    # client = pymongo.MongoClient(os.environ['MONGOHOST'], 27017, connect=False)
+    client = pymongo.MongoClient('mongodb://3.220.111.222:27017/')
+    client.admin.authenticate("aircas", "aircas@2018", mechanism='SCRAM-SHA-1')
+    # db = client.natural_disaster
+    db = client['submit']
+    # db.authenticate(name='aircas',password='aircas@2018')
 
     # Redis数据库
     r = redis.StrictRedis(host=os.environ['REDISHOST'], port=6379, db=0)
+    # r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 
     return got, db, r
