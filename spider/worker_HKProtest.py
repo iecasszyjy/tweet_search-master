294d7ed4d7abd4773484831ff31bf52738ee7842
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2019-10-22 17:28:19 +0800

    Update worker_HKProtest.py

--- spider/worker_HKProtest.py
+++ spider/worker_HKProtest.py
@@ -6,10 +6,10 @@ import json
 import multiprocessing
 from multiprocessing import Pool
 
-from Config_2019 import get_noau_config, getMongoClient, closeMongoClient
+from Config_2019 import getGot, getMongoClient, closeMongoClient
 
-def advance_search_dataset(q, f, num, actionId):  # 获取推文，放入MongoDB数据库
-    got, db = get_noau_config()  # 文件和数据库配置
+def advance_search_dataset(q, f, num, actionId, db):  # 获取推文，放入MongoDB数据库
+    got = getGot()  # 文件配置
     collection = db.dataset
     tweetCriteria = got.manager.TweetCriteria().setQuerySearch(q).setTweetType(f).setMaxTweets(num)
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
@@ -17,15 +17,15 @@ def advance_search_dataset(q, f, num, actionId):  # 获取推文，放入MongoDB
         if collection.find_one({'id': tweet['id']}) is None:  # 查看推文是否已存在，若不存在则放入数据库
             collection.insert_one({'id': tweet['id'], 'tweet': tweet, 'actionId': actionId, 'f': f, 'q': q})
 
-def run_dataset_task(actionId, q, f, num):
+def run_dataset_task(actionId, q, f, num, db):
     try:
         if type(f) != list:
-            advance_search_dataset(q, f, num, actionId)
+            advance_search_dataset(q, f, num, actionId, db)
         else:
             # 多进程获取推文
             pool = Pool(processes=multiprocessing.cpu_count())
             # python官方建议：废弃apply，使用apply_async（使用apply在这里会出错）
-            [pool.apply_async(advance_search_dataset, (q, fi, num, actionId)) for fi in f]
+            [pool.apply_async(advance_search_dataset, (q, fi, num, actionId, db)) for fi in f]
             # [pool.apply(advance_search_dataset, (q, f, num, event_id)) for f in message_data['f']]
             pool.close()
             pool.join()
@@ -37,20 +37,23 @@ def run_dataset_task(actionId, q, f, num):
 if __name__ == '__main__':
     print '2019HongKong_protest craw_worker start!'
     c = getMongoClient()
-    d = c['2019HongKong_protest']
-    searchInfo = d.searchInfo
+    db = c['2019HongKong_protest']
+    searchInfo = db.searchInfo
     searchInfoResult = searchInfo.find()
     closeMongoClient(c)
+    c = getMongoClient()
+    db = c['2019HongKong_protest']
     for item in searchInfoResult:
         print '2019HongKong_protest craw_worker process!'
         actionId = item['id']
         q = item['q']
         f = item['f']
         num = item['num']
-        craw = run_dataset_task(actionId, q, f, num)  # 获取推文
+        craw = run_dataset_task(actionId, q, f, num, db)  # 获取推文
         if craw:
             # 添加查询日志
-            d.dataset_log.insert_one({'actionId': actionId, 'q': q, 'f': f, 'num': num, 'status': 1})
+            db.dataset_log.insert_one({'actionId': actionId, 'q': q, 'f': f, 'num': num, 'status': 1})
         else:
-            d.dataset_log.insert_one({'actionId': actionId, 'q': q, 'f': f, 'num': num, 'status': 0})
+            db.dataset_log.insert_one({'actionId': actionId, 'q': q, 'f': f, 'num': num, 'status': 0})
+    closeMongoClient(c)
     print '2019HongKong_protest craw_worker finish!'
