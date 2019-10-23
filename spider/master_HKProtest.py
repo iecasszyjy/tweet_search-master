261ef0042fa31679406a51dcacc69f0db4738d4a
iecasszyjy <42063972+iecasszyjy@users.noreply.github.com>
2019-10-23 17:15:07 +0800

    Update master_HKProtest.py

--- spider/master_HKProtest.py
+++ spider/master_HKProtest.py
@@ -1,34 +1,32 @@
 # -*- coding: utf-8 -*
 # 获得爬取推文所需信息
 # import json
-from Config_2019 import get_noau_config
-# from datetime import datetime, timedelta
-
-_, db = get_noau_config()  # 数据库配置
+from Config_2019 import getMongoClient, closeMongoClient
 
 def get_task():
     event_list = list(db.event_list.find())
     stime, etime, locations, triggers = '','','',''
-    print('event_list num:' + str(len(event_list)))
+    print('2019HongKong_protest event_list num:' + str(len(event_list)))
+    client = getMongoClient()
+    db = client['2019HongKong_protest']
+    searchInfo = db.searchInfo
     for event in event_list:
         print(event)
-        print(type(event))
+        # print(type(event))
         stime = event['event']['stime']
         etime = event['event']['etime']
-        locations = event['event']['locations']
+        location = event['event']['location']
         triggers = event['event']['triggers']
-        q = '(' + locations + ')' + ' ' + '(' + triggers + ')' + ' ' + 'since:' + stime + ' ' + 'until:' + etime
+        topics = event['event']['topics']
+        q = '(' + location + ')' + ' ' + triggers + ' ' + topics + ' ' + 'since:' + stime + ' ' + 'until:' + etime
         actionId = event['id']
         message = {'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 50000, 'action_id': actionId}
-        print(q)
         print(message)
-    # # 把获取推文所需信息放入Redis数据库
-    # r.rpush('2019HongKong_protest', json.dumps(message))
-    # 把获取推文所需的特定格式信息存入mongo
-        searchInfo = db.searchInfo
+        # 把获取推文所需的特定格式信息存入mongo
         if searchInfo.find_one({'id': actionId}) is None:
             searchInfo.insert_one({'id': actionId, 'q': q, 'f': ['&f=news', '', '&f=tweets'], 'num': 50000})
-            print('推文查询语句存储到mongo/2019HongKong_protest/searchInfo完成')
+    print('推文查询语句存储到mongo/2019HongKong_protest/searchInfo完成')
+    closeMongoClient(client)
 
 if __name__ == '__main__':
     get_task()
